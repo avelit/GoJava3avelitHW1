2cc50d2e5620bd865bc693eb45f20025c1302299
Velytchenko Oleksandr <avelit@ukr.net>
2015-03-12 17:20:00 +0200

    Optional

--- src/com/goit/gojava3/hw1/Optional.java
+++ src/com/goit/gojava3/hw1/Optional.java
@@ -9,6 +9,13 @@ public class Optional {
 
         int[] intArray = { 2, 3, 2, 4, 3, 7, 2, 3, 4, 4 };
 
+        findByDictionary(intArray);
+        
+        findByRadix3(intArray);
+
+    }
+
+    public static void findByDictionary(int[] intArray) {
         HashMap<Integer, Integer> hash = new HashMap<>();
         for (Integer el : intArray) {
             Integer amount = hash.get(el);
@@ -20,10 +27,36 @@ public class Optional {
         }
         for (Map.Entry<Integer, Integer> h : hash.entrySet()) {
             if (h.getValue() == 1) {
-                System.out.println("result " + h.getKey());
+                System.out.println("result 0: " + h.getKey());
                 break;
             }
         }
-
     }
+    public static void findByRadix3(int[] intArray) {
+        String result = "0";
+        final int radix = 3;
+        for (Integer el : intArray) {
+            String elRadix3 = Integer.toString(el, radix);
+            result = sumByModule3(result,elRadix3);
+        }
+        System.out.println("result 1: " + Integer.parseInt(result, radix));
+    }
+
+    public static String sumByModule3(String str1,String str2) {
+        String localResult = "";
+        int minLenght = Math.min(str1.length(), str2.length());
+        // add 0..0 to begin
+        if (minLenght < str2.length()) {
+            str1 = "000000000000000000000000000".substring(0, str2.length() - minLenght) + str1;
+        }
+        else if (minLenght < str1.length()){
+            str2 = "000000000000000000000000000".substring(0, str1.length() - minLenght) + str2;
+        }
+        // now both srt have same length
+        for (int i = 0; i < str2.length(); i++) {
+            localResult += String.valueOf(((Integer.parseInt(str1.substring(i,i + 1)) + Integer.parseInt(str2.substring(i,i + 1))) % 3));
+        }
+        return localResult;
+    }
+
 }
